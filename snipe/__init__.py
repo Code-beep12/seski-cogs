5be1efd95f190edc64c648d8df62a061b14c00ce
Code-beep12 <77047431+Code-beep12@users.noreply.github.com>
2022-01-08 18:35:54 +0300

    Update __init__.py

--- snipe/__init__.py
+++ snipe/__init__.py
@@ -2,7 +2,7 @@ from discord.ext import commands
 import discord
 import time
 from discord.member import Member
-
+from .snipe import snipe
 
 def setup(bot: commands.Bot):
         bot.add_cog(snipe(bot))
