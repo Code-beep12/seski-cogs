d3db122bb5030d429f85f5461dd783c9c88b7d04
Code-beep12 <77047431+Code-beep12@users.noreply.github.com>
2022-01-08 17:40:51 +0300

    Update snipe.py

--- snipe/snipe.py
+++ snipe/snipe.py
@@ -2,7 +2,7 @@ from discord.ext import commands
 import discord
 import time
 from discord.member import Member
-
+from redbot.core import commands
 
 class snipe(commands.Cog):
     def __init__(self, bot: commands.Bot):
